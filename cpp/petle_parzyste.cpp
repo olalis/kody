d9e555e93926bd8edd490151cebbbefa8e584908
olalis <aleksandralis2000@gmail.com>
2017-10-17 20:02:36 +0200

    zadanie podzielne przez 3

--- cpp/petle_parzyste.cpp
+++ cpp/petle_parzyste.cpp
@@ -14,15 +14,31 @@ int main(int argc, char **argv)
     
     cout << "Podaj m (10-99)" << endl;
     cin >> m;
-    cout << "Podaj n (10-99)(n < m)" << endl;
+    cout << "Podaj n (10-99)(n > m)" << endl;
     cin >> n;
     
-    while (m < n && m < 10 && m > 99 && n < 10 && n > 99)
+    while (m > n || m < 10 || m > 99 || n < 10 || n > 99)
     {
         cout << "Podaj m (10-99)" << endl;
         cin >> m;
-        cout << "Podaj n (10-99)(n < m)" << endl;
+        cout << "Podaj n (10-99)(n > m)" << endl;
         cin >> n;
+    }
+    while (m <= n)
+    {
+        if (m % 3 == 0)
+        {
+            cout << m << endl;
+        }
+        else if ((m+1) % 3 == 0)
+        {
+            cout << m+1 << endl;
+        }
+        else if ((m+2) % 3 == 0)
+        {
+            cout << m+2 << endl;
+        }
+        m = m+3;
     }
     
         
