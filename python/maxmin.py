a9c2ebe4362278846581d75157b6278f8f7e93db
olalis <ola.lis.2000@wp.pl>
2017-11-24 11:41:26 +0100

    minmax

--- python/maxmin.py
+++ python/maxmin.py
@@ -3,6 +3,22 @@
 from random import randint
 
 
+def minmax(lista):
+    lmin = []
+    lmax = []
+    indeks = 0
+    for i in range(int(len(lista) / 2)):
+        if lista[indeks] > lista[indeks + 1]:
+            lmin.append(lista[indeks + 1])
+            lmax.append(lista[indeks])
+            indeks += 2
+        else:
+            lmin.append(lista[indeks])
+            lmax.append(lista[indeks + 1])
+            indeks += 2
+    return lmin, lmax
+
+
 def minimum(lista):
     min = lista[0]
     for i in lista:
@@ -30,6 +46,8 @@ def main(args):
     ile = 20
     zakres = 50
     lista = losuj(ile, zakres)
+    assert minimum([7, 3, 17, 56, 0]) == 0
+    assert maksimum([7, 3, 17, 56, 0]) == 56
     print(lista)
     print("Minimum: ", minimum(lista))
     print("Maksimum: ", maksimum(lista))
