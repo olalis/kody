67c2251746204d57e28ead9a1a0683fb085373cb
olalis <aleksandralis2000@gmail.com>
2018-11-28 09:40:45 +0100

    kwerendy orm

--- baza/uczniowie/kwerendy_orm.py
+++ baza/uczniowie/kwerendy_orm.py
@@ -5,6 +5,43 @@
 import os
 from uczniowie_model import *
 
+def kw08():
+    """Lista nazwisk uczniów i nazw klas"""
+    query = (Uczen
+        .select(Uczen.nazwisko, Uczen.klasa.klasa)
+        .join(Klasa) #Połączenie tabel
+    )
+    
+    for obj in query:
+        print(obj.nazwisko, obj.klasa.klasa)
+        
+def kw09():
+    """Lista klas i ilość uczniów"""
+    query = (Uczen
+        .select(fn.Count(Uczen.id).alias('ilu'), Uczen.klasa.klasa)
+        .join(Klasa) #Połączenie tabel
+        .group_by(Klasa)
+        .order_by(SQL('ilu').asc())
+    )
+    
+    for obj in query:
+        print(obj.klasa.klasa, obj.ilu)
+        
+def kw10():
+    """Lista przedmiotów i ilość ocen"""
+    query = (Przedmiot
+        .select(fn.Count(Ocena.uczen).alias('ile'), Przedmiot.przedmiot)
+        .join(Ocena) #Połączenie tabel
+        .group_by(Przedmiot)
+        .order_by(SQL('ile').asc())
+    )
+    
+    for obj in query:
+        print(obj.przedmiot, obj.ile)
+
+def kw11():
+    """Suma wszystkich ocen"""
+
 
 def main(args):
     baza.connect()  # połączenie z bazą
@@ -20,8 +57,9 @@ def main(args):
         
     ]
     
-    for obj in eval(kwerendy[6]):
-        print(obj.nazwisko, obj.imie, obj.egz_mat)
+    #for obj in eval(kwerendy[6]):
+    #    print(obj.nazwisko, obj.imie, obj.egz_mat)
+    kw10()
     
     baza.commit()
     baza.close()
