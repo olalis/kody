34c8e67e960c4e9c770ed0550eeafeddfe893d91
olalis <aleksandralis2000@gmail.com>
2018-12-17 14:19:04 +0100

    views

--- flask_chat/baza.py
+++ flask_chat/baza.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  baza.py
 import csv
-import sqlite3
-import os
+import os.path
 from modele import *
 
+
 def czy_jest(plik):
-    """Funkcja sprawdza czy plik istnieje na dysku"""
+    """F. sprawdza, czy plik istnieje na dysku"""
     if not os.path.isfile(plik):
         print("Plik {} nie istnieje!".format(plik))
         return False
     return True
 
-def czytaj_dane(plik, separator=";"):
-    dane = [] #pusta lista na rekordy
+def czytaj_dane(plik, separator=","):
+    dane = []  # pusta lista na rekordy
     
     if not czy_jest(plik):
         return dane
-        
-    with open(plik, 'r', newline="", encoding="utf-8") as plikcsv:
+    
+    with open(plik, 'r', newline='', encoding='utf-8') as plikcsv:
         tresc = csv.reader(plikcsv, delimiter=separator)
         for rekord in tresc:
             dane.append(tuple(rekord))
     return dane
 
+
 def dodaj_dane(dane):
     
     for model, plik in dane.items():
         pola = [pole for pole in model._meta.fields]
-        pola.pop(0) #usuwamy pola id
+        pola.pop(0)  # usuwamy pola id
         print(pola)
         
-        wpisy = czytaj_dane(plik + '.csv')
-        model.insert_many(wpisy, fields = pola).execute()
-    
+        wpisy = czytaj_dane(plik + '.csv', ';')
+        model.insert_many(wpisy, fields=pola).execute()
+
 
 def main(args):
-    baza_plik = "baza.db"
-    if os.path.exists(baza_plik):
-        os.remove(baza_plik)
-    baza.connect()  # połączenie z bazą
+    if os.path.exists(baza_nazwa):
+        os.remove(baza_nazwa)
+    baza.connect()  # poĹÄczenie z bazÄ
     baza.create_tables([Kategoria, Pytanie, Odpowiedz])  # tworzymy tabele
 
     dane = {
@@ -58,3 +60,4 @@ def main(args):
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
+
